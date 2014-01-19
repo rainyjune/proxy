d59a67d40820ff6de255de95e9a0acfe84fee11c
rainyjune <rainyjune@live.cn>
2014-01-19 14:41:34 +0800

    Add page navigation support.

--- functions.php
+++ functions.php
@@ -5,8 +5,13 @@ function generateLocalFile($url)
   $title = $html->find('title', 0)->innertext;
   $images = $html->find('.main img');
   $menuElements = $html->find('#menu_box ul');
+  $listElements = $html->find('.main a');
+  $listPagesElements = $html->find('.box_page a,.box_page li[class],span.pageinfo');
   $menuArray = getMenus($menuElements);
-  saveFile($url, GBK2UTF8($title), $menuArray, $images);
+  $listArray = getList($listElements);
+  $listPageArray = getListPages($listPagesElements);
+  //var_dump($listPageArray);exit;
+  saveFile($url, GBK2UTF8($title), $menuArray, $images, $listArray, $listPageArray);
 }
 function getMenus($menuElements)
 {
@@ -28,6 +33,31 @@ function getMenus($menuElements)
   return $menuArray;
 }
 
+function getListPages($listPagesElements)
+{
+  //var_dump($listPagesElements);exit;
+  $result = array('pages'=>array(),'pagesInfo'=>'');
+  foreach ($listPagesElements as $key => $element) {
+    if ($element->tag =='li') {
+      $result['pages'][GBK2UTF8($element->innertext)] = '#';
+    } else if($element->tag =='a') {
+      $result['pages'][GBK2UTF8($element->innertext)] = $element->href ? $element->href : '#';
+    } else if ($element->tag =='span') {
+      $result['pagesInfo'] = strip_tags(GBK2UTF8($element->innertext));
+    }
+  }
+  return $result;
+}
+
+function getList($listElements)
+{
+  $listArray = array();
+  foreach ($listElements as $link) {
+    $listArray[GBK2UTF8($link->innertext)] = $link->href;
+  }
+  return $listArray;
+}
+
 function GBK2UTF8($str)
 {
   return iconv('GBK', 'UTF-8', $str);
@@ -38,7 +68,7 @@ function getFileName($url)
   return 'cache/'.md5($url).".html";
 }
 
-function saveFile($url,$title, $menus, $images)
+function saveFile($url,$title, $menus, $images, $listArray, $listPageArray)
 {
   $fileName =getFileName($url);
 
@@ -53,6 +83,12 @@ function saveFile($url,$title, $menus, $images)
 
   // Add pictures
   addPictures($doc, $container, $images);
+
+  // Add List
+  addList($doc, $listArray);
+
+  // Add List pages navigation
+  addListPages($doc, $listPageArray);
 
   // Save to a HTML file
   $doc->saveHTMLFile($fileName);
@@ -78,6 +114,38 @@ function setTitle($doc, $title)
   {
     $titleNode->nodeValue = $title;
   }
+}
+
+function addListPages($doc, $listPageArray)
+{
+  $container = $doc->getElementById('pageList');
+  $pages = $listPageArray['pages'];
+  foreach ($pages as $text => $link) {
+    $li = $doc->createElement('li');
+    $a = $doc->createElement('a');
+    $a->nodeValue = $text;
+    $a->setAttribute("href", $link);
+    $li->appendChild($a);
+    $container->appendChild($li);
+  }
+  $li = $doc->createElement('li');
+  $li->nodeValue = $listPageArray['pagesInfo'];
+  $container->appendChild($li);
+}
+
+function addList($doc, $listArray)
+{
+  $listContainer = $doc->getElementById('listContainer');
+  $ul = $doc->createElement('ul');
+  foreach ($listArray as $text => $link) {
+    $li = $doc->createElement('li');
+    $a = $doc->createElement('a');
+    $a->nodeValue = $text;
+    $a->setAttribute("href", $link);
+    $li->appendChild($a);
+    $ul->appendChild($li);
+  }
+  $listContainer->appendChild($ul);
 }
 
 function addMenus($doc, $menus)
