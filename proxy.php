67022b544d4b9995531639c4e41838d03bb48aec
rainyjune <rainyjune@live.cn>
2014-01-19 13:13:01 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -6,18 +6,15 @@ if ($url)
 {
   $md5FileName = getFileName($url);
 
-  // If cache exists, read from cache.
-  if (file_exists($md5FileName))
+  // If cache does not exists, fetch from remote server and then write into cache.
+  if (!file_exists($md5FileName))
   {
-    echo file_get_contents($md5FileName);
-    exit;
+    $html = file_get_html($url);
+    $title = $html->find('title', 0)->innertext;
+    $images = $html->find('.main img');
+    $menuElements = $html->find('#menu_box ul');
+    $menuArray = getMenus($menuElements);
+    saveFile($url, GBK2UTF8($title), $menuArray, $images);
   }
-
-  // Get file content from remote server.
-  $html = file_get_html($url);
-  $title = $html->find('title', 0)->innertext;
-  $images = $html->find('.main img');
-  $menuElements = $html->find('#menu_box ul');
-  $menuArray = getMenus($menuElements);
-  saveFile($url, GBK2UTF8($title), $menuArray, $images);
+  echo file_get_contents($md5FileName);
 }
